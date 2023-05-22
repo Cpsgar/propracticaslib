3884cd0ff9d4aa93cd7ba79e28e5e2a8ecbdbe1d
Cpsg <cesardim45@gmail.com>
2023-05-22 18:57:15 +0200

    Interzaz y entidad Alumno

---
+++ src/main/java/es/mdef/propracticaslib/Alumno.java
@@ -0,0 +1,11 @@
+package es.mdef.propracticaslib;
+
+public interface Alumno {
+	
+	String getNombre();
+	String getApellidos();
+	String getDni();
+	Integer getPromocionDim();
+	String getInstitucion();
+	Practica getPracticaAsignada();
+}
